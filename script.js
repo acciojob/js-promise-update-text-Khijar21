4f0171eb240b0062d2b7e2208d64795737036758
Khijar Sheikh <120732221+Khijar21@users.noreply.github.com>
2023-06-27 19:01:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function prom(){
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		},1000)
+	})
+}
+
+function updateHtmlElement(elementId,text){
+	const element = document.getElementById('output');
+	if(element){
+		element.textContent = text;
+	}
+}
+
+const  output = "output"
+prom().then((result) => {
+	updateHtmlElement(output,result)
+});
